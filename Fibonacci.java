8505a283e6a81c7d1ad8b09f8508efe1ef9220d7
Dhrubajyoti Biswas <dhrubajyotibiswasDribll@outlook.com>
2025-03-21 16:28:59 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,10 @@
+public class Fibonacci{
+    public static void main(){
+        int a,b=1;
+        for(a=0;a<(b+1);a++){
+            int c = a + b;
+            System.out.println(a+"+"+b+"="+c);
+            b++;
+        }
+    }
+}
