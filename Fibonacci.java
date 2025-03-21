c88945772d115dd2aa70ccda285a3576b9264135
Dhrubajyoti Biswas <dhrubajyotibiswasDribll@outlook.com>
2025-03-21 20:14:56 +0530

    Update Fibonacci.java
    The output of fibonacci series might come as negative integers from the no. -6246583658587674878 as long datatype overflows its capacity limit.

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,10 +1,17 @@
 public class Fibonacci{
     public static void main(){
-        int a,b=1;
-        for(a=0;a<(b+1);a++){
-            int c = a + b;
+        long a = 0;
+        long b = 1;
+        long c,count=1;
+        while(count>0){
+            c = a + b;
             System.out.println(a+"+"+b+"="+c);
-            b++;
+            a = b;
+            b = c;
+            
+            count++;
+
+            // The output of fibonacci series might come as negative integers from the no. -6246583658587674878 as long datatype overflows its capacity limit.
         }
     }
 }
